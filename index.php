3c2b8e3857a045c38f2f2311e5c54985b7b9e583
Tim De Pauw <hi@tmdpw.eu>
2014-10-19 22:43:39 +0200

    Moving index out of document root

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 error_reporting(0);
 
 try {
-  require_once dirname(__FILE__) . '/../vendor/autoload.php';
-  foreach (glob(dirname(__FILE__) . '/../lib/*.php') as $lib) {
+  require_once dirname(__FILE__) . '/vendor/autoload.php';
+  foreach (glob(dirname(__FILE__) . '/lib/*.php') as $lib) {
     require_once $lib;
   }
   session_start();
