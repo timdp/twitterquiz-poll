1a4d4940583f0aee64bbeab1c93ab5099dd6898a
Tim De Pauw <hi@tmdpw.eu>
2016-03-19 18:07:53 +0100

    Support longer user IDs

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "votes" (
-  "user_id" integer NOT NULL,
+  "user_id" bigint NOT NULL,
   "screen_name" varchar(20) DEFAULT NULL,
   "vote" integer NOT NULL,
   "when" timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
